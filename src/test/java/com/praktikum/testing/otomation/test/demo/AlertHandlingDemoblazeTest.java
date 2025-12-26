0c7ba65676e45ec9263443f76d7a2705e66ee9ef
Richard Bagus Efendi <rbagusefendi@gmail.com>
2025-12-26 17:04:29 +0700

    create project

--- src/test/java/com/praktikum/testing/otomation/test/demo/AlertHandlingDemoblazeTest.java
+++ src/test/java/com/praktikum/testing/otomation/test/demo/AlertHandlingDemoblazeTest.java
@@ -1,4 +1,30 @@
 package com.praktikum.testing.otomation.test.demo;
 
-public class AlertHandlingDemoblazeTest {
+import com.praktikum.testing.otomation.pages.*;
+import com.praktikum.testing.otomation.utils.TestDataGenerator;
+import org.testng.annotations.Test;
+
+public class AlertHandlingDemoblazeTest extends BaseTest {
+
+    @Test(priority = 1, description = "Verifikasi alert saat registrasi berhasil")
+    public void testRegistrationAlert() {
+        test = extent.createTest("Alert - Registration Success");
+        HomePage home = new HomePage(driver);
+        RegistrationPage reg = new RegistrationPage(driver);
+
+        home.clickRegister();
+        reg.registerUser(TestDataGenerator.generateRandomUsername(), "AlertPass123");
+        test.pass("Alert registrasi berhasil ditangani.");
+    }
+
+    @Test(priority = 2, description = "Verifikasi alert saat menambah produk")
+    public void testAddToCartAlert() {
+        test = extent.createTest("Alert - Add to Cart Success");
+        HomePage home = new HomePage(driver);
+        ProductPage product = new ProductPage(driver);
+
+        home.selectFirstProduct();
+        product.addToCart();
+        test.pass("Alert 'Product added' berhasil ditangani.");
+    }
 }
--- src/test/java/com/praktikum/testing/otomation/test/demo/DemoblazeNavigationAndFlowTest.java
+++ src/test/java/com/praktikum/testing/otomation/test/demo/DemoblazeNavigationAndFlowTest.java
@@ -1,4 +1,33 @@
 package com.praktikum.testing.otomation.test.demo;
 
-public class DemoblazeNavigationAndFlowTest {
+import com.praktikum.testing.otomation.pages.HomePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoblazeNavigationAndFlowTest extends BaseTest {
+
+    @Test(priority = 1, description = "Navigasi antar kategori produk")
+    public void testCategoryNavigation() {
+        test = extent.createTest("Flow - Category Switching");
+        HomePage home = new HomePage(driver);
+
+        test.info("Membuka halaman Home");
+        home.goToHomePage();
+        Assert.assertTrue(driver.getCurrentUrl().contains("index.html"));
+
+        test.info("Membuka menu Cart via navigasi");
+        home.goToCart();
+        Assert.assertTrue(driver.getCurrentUrl().contains("cart.html"));
+        test.pass("Navigasi antar menu utama lancar.");
+    }
+
+    @Test(priority = 2, description = "Verifikasi logo kembali ke halaman utama")
+    public void testLogoRedirection() {
+        test = extent.createTest("Flow - Logo Redirect");
+        HomePage home = new HomePage(driver);
+        home.goToCart();
+        home.goToHomePage();
+        Assert.assertTrue(driver.getCurrentUrl().contains("index.html"));
+        test.pass("Klik Logo berhasil mengarahkan kembali ke Home.");
+    }
 }
